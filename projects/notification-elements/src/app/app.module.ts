5ad4ac92535e9380eea221791d41ec6934a338b5
Bishoy Louka <Bishoy.Louka@verizonconnect.com>
2019-12-19 16:43:31 +0000

    first angular element

--- projects/notification-elements/src/app/app.module.ts
+++ projects/notification-elements/src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule,
+  Injector
+} from '@angular/core';
+import {
+  createCustomElement
+} from '@angular/elements';
+import {
+  NotificationDialogComponent
+} from 'projects/notification-elements/notification-dialog/notification-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    NotificationDialogComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {
+
+  }
+
+  ngDoBootstrap() {
+    const notificationDialogCE = createCustomElement(NotificationDialogComponent, {
+      injector: this.injector
+    });
+    customElements.define('notification-dialog', notificationDialogCE);
+  }
+
+}
--- projects/notification-elements/src/main.ts
+++ projects/notification-elements/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'zone.js'; // TODO: disable that by build if we're in an angular APP
+
 if (environment.production) {
   enableProdMode();
 }
